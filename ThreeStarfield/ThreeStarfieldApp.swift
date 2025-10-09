2f7b8b19559c02d57eed76427093a90bf771df4f
Shailesh Patel <shailesh001@mac.com>
2025-10-09 15:34:28 +0100

    Initial Commit

---
+++ ThreeStarfield/ThreeStarfieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreeStarfieldApp.swift
+//  ThreeStarfield
+//
+//  Created by Shailesh Patel on 09/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ThreeStarfieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
